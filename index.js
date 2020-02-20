ba6511d767ed724e170af066968b8bb717d800b9
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-02-20 11:19:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 /* Timer */
 
-function startTimer(duration,display) {
-  var timer = duration; minutes; seconds;
-  setInterval(function() {
-    minutes = parseInt(timer/60,10);
-    seconds = parseInt(timer%60,10);
-    minutes = minutes<10?"0"+minutes:minutes;seconds=seconds<10?"0"+seconds:seconds;
-    display.textContent=minutes+":"+seconds;
-  if(--timer < 0 ) {
-    timer=duration;
-  }
- } ,1000);
+function startTimer(duration, display) {
+    var timer = duration,
+        minutes, seconds;
+    setInterval(function() {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = minutes + ":" + seconds;
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
 }
-
-window.onload=function() {
-var fiveMinutes = 60*5,
-display = document.querySelector('#stopwatch');
-startTimer(fiveMinutes,display);
+window.onload = function() {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#stopwatch');
+    startTimer(fiveMinutes, display);
 };
 
-/* Country state */
+/* ---------- Country state -----------*/
 <script type="text/javascript">
   $(document).ready(function () {
     initializeCountryState('country', 'state', 'state-container', 'zip-code', 'class="form-control" data-toggle="tooltip" data-placement="auto left" title="State" data-validation="required"',false);
